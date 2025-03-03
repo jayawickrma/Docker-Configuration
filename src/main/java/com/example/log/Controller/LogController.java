cef6b92dc3435dcea54ba7fc15e9bb0624a03a3a
jayawickrama <nishantharaka2004@gmail.com>
2025-03-03 11:21:38 +0530

    log printed done

--- src/main/java/com/example/log/Controller/LogController.java
+++ src/main/java/com/example/log/Controller/LogController.java
@@ -1,9 +1,8 @@
-package com.example.log;
+package com.example.log.Controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
